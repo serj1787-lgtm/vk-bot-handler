45012106e438ff334090e8db75fb2d097d30db40
serj1787-lgtm <serj1787@gmail.com>
2025-11-05 16:29:04 +0300

    Add files via upload

---
+++ vk-callback/api/callback.js
@@ -0,0 +1,18 @@
+javascript
+export default function handler(req, res) {
+  console.log('Method:', req.method);
+  console.log('Body:', req.body);
+  
+  if (req.method === 'POST') {
+    const body = req.body;
+    
+    if (body.type === 'confirmation') {
+      console.log('Confirmation request for group:', body.group_id);
+      return res.status(200).send('74173fc9');
+    }
+    
+    return res.status(200).send('ok');
+  } 
+  
+  return res.status(405).send('Method Not Allowed');
+}
